d01f3234e5625072ad19f94da1bebcf01bbf56e5
Detjon <detjon.mataj@gmail.com>
2022-07-10 21:03:37 +0200

    Grammar correction in a comment

--- AutoDereferencing-dot-operator/main.c
+++ AutoDereferencing-dot-operator/main.c
@@ -35,12 +35,12 @@ int main(void)
 
     ///! Calling the function sum with the address of foo as argument
     ///! This function will add the values of f1 and f2 and print the sum
-    ///! It will access struct fields with member access operator '->' in the regular way (not Auto Deferecing with dot operator)
+    ///! It will access struct fields with member access operator '->' in the regular way (not Auto Dereferencing with dot operator)
     sum(&foo);
 
     ///! Calling the function _sum with the address of foo as argument
     ///! This function will add the values of f1 and f2 and print the sum
-    ///! It will access struct fields with member access operator '.' using Auto Deferecing with dot operator Patch
+    ///! It will access struct fields with member access operator '.' using Auto Dereferencing with dot operator Patch
     ///! This feature is available only for the Tiny C Compiler with my patch
     ///! The compiler patch it's backward compatible with the legacy code, but the codes that uses 'Auto Dereferencing' with dot operator can't be compiled with the legacy compilers
     _sum(&foo);
